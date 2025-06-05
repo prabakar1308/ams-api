3d62095b6b6775f3c188e0beaf0122b78134928b
Prabakaran <prabakar1308@gmail.com>
2025-06-05 11:11:20 +0530

    comment cors config

--- src/main.ts
+++ src/main.ts
@@ -35,9 +35,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.enableCors({
-    origin: '*',
-  });
+  // app.enableCors({
+  //   origin: '*',
+  // });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
